fb4df2b3cbf2aba465a32b7fa14023121610650b
Joshua Porter <jmporter@ksu.edu>
2019-04-01 12:02:34 -0500

    commit

---
+++ Ksu.Cis300.PriorityQueueLibrary/LeftistTree.cs
@@ -0,0 +1,81 @@
+﻿/* BinaryTreeNode.cs
+ * Author: Rod Howell */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KansasStateUniversity.TreeViewer2;
+
+namespace Ksu.Cis300.PriorityQueueLibrary
+{
+    /// <summary>
+    /// An immutable generic binary tree node that can draw itself.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements stored in the tree.</typeparam>
+    public partial class LeftistTree<T> : ITree
+    {
+        /// <summary>
+        /// Stores the null path length
+        /// </summary>
+        private int _nullPathLength;
+
+        /// <summary>
+        /// Gets the data stored in this node.
+        /// </summary>
+        public T Data { get; }
+
+        /// <summary>
+        /// Gets this node's left child.
+        /// </summary>
+        public LeftistTree<T> LeftChild { get; }
+
+        /// <summary>
+        /// Gets this node's right child.
+        /// </summary>
+        public LeftistTree<T> RightChild { get; }
+
+        /// <summary>
+        /// Constructs a LeftistTreeNode with the given data, left child, and right child.
+        /// </summary>
+        /// <param name="data">The data stored in the node.</param>
+        /// <param name="left">The left child.</param>
+        /// <param name="right">The right child.</param>
+        public LeftistTree(T data, LeftistTree<T> left, LeftistTree<T> right)
+        {
+            Data = data;
+            int leftNullPath = NullPathLength(left);
+            int rightNullPath = NullPathLength(right);
+            if (rightNullPath < leftNullPath)
+            {
+                LeftChild = left;
+                RightChild = right;
+                _nullPathLength = NullPathLength(right) + 1;
+            }
+            else
+            {
+                LeftChild = right;
+                RightChild = left;
+                _nullPathLength = NullPathLength(left) + 1;
+            }
+            
+        }
+
+        /// <summary>
+        /// Rwturns the value stored in the null path length field
+        /// </summary>
+        /// <param name="t">The leftist tree to find the null path length of</param>
+        /// <returns>The null path length</returns>
+        public static int NullPathLength(LeftistTree<T> t)
+        {
+            if (t == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return t._nullPathLength;
+            }
+        }
+    }
+}
---
+++ Ksu.Cis300.PriorityQueueLibrary/MinPriorityQueue.cs
@@ -0,0 +1,106 @@
+﻿/* MinPriorityQueue.cs
+ * Author: Joshua Porter
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.PriorityQueueLibrary
+{
+    public class MinPriorityQueue<TPriority, TValue> where TPriority: IComparable<TPriority> 
+    {
+        /// <summary>
+        /// A leftist heap storing the elements and their priorities.
+        /// </summary>
+        private LeftistTree<KeyValuePair<TPriority, TValue>> _elements = null;
+
+        /// <summary>
+        /// Gets the number of elements.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Gets the minimum priority
+        /// </summary>
+        public TPriority MinimumPriority
+        {
+            get
+            {
+                // Code to check the error condition and return the minimum priority.
+                if (Count == 0)
+                {
+                    throw new InvalidOperationException();
+                    
+                }
+                return _elements.Data.Key;
+            }
+        }
+
+        /// <summary>
+        /// Adds the given element with the given priority.
+        /// </summary>
+        /// <param name="p">The priority of the element.</param>
+        /// <param name="x">The element to add.</param>
+        public void Add(TPriority p, TValue x)
+        {
+            LeftistTree<KeyValuePair<TPriority, TValue>> node =
+                new LeftistTree<KeyValuePair<TPriority, TValue>>(new KeyValuePair<TPriority, TValue>(p, x), null, null);
+            _elements = Merge(_elements, node);
+            Count++;
+        }
+
+        /// <summary>
+        /// Merges the given leftist heaps into one leftist heap.
+        /// </summary>
+        /// <param name="h1">One of the leftist heaps to merge.</param>
+        /// <param name="h2">The other leftist heap to merge.</param>
+        /// <returns>The resulting leftist heap.</returns>
+        public static LeftistTree<KeyValuePair<TPriority, TValue>> Merge(LeftistTree<KeyValuePair<TPriority, TValue>> h1,
+            LeftistTree<KeyValuePair<TPriority, TValue>> h2)
+        {
+            if (h1 == null)
+            {
+                return h2;
+            }
+            else if (h2 == null)
+            {
+                return h1;
+            }
+            else
+            {
+                if (h1.Data.Key.CompareTo(h2.Data.Key) < 0)
+                {
+                    LeftistTree<KeyValuePair<TPriority, TValue>> newTree = new LeftistTree<KeyValuePair<TPriority, TValue>>(h1.Data, h1.LeftChild, Merge(h2, h1.RightChild));
+                    return newTree;
+                }
+                else
+                {
+                    LeftistTree<KeyValuePair<TPriority, TValue>> newTree = new LeftistTree<KeyValuePair<TPriority, TValue>>(h2.Data, h2.LeftChild, Merge(h1, h2.RightChild));
+                    return newTree;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Rmoves the item with minimum priority
+        /// </summary>
+        /// <returns>The minimum priority</returns>
+        public TValue RemoveMinimumPriority()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                TValue value = _elements.Data.Value;
+                _elements = Merge(_elements.LeftChild, _elements.RightChild);
+                Count--;
+                return value;
+            }
+            
+        }
+    }
+}
